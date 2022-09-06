a2ce7a384551f5fb7d5d9edc6d3ed37b4c4acfd2
Brian Lin <blin00712@gmail.com>
2022-09-06 19:15:52 -0400

    9/6 heap sort

--- src/algorithms/heapSort.js
+++ src/algorithms/heapSort.js
@@ -4,70 +4,55 @@ const hSort = (bars, barSize) => {
     if (bars.length <= 1) return bars
 
     const visualIndexArr = []
-    heapSort(barSize - 1, bars, visualIndexArr)
+    heapSort(barSize, bars, visualIndexArr)
     return visualIndexArr;
 }
 
 const heapSort = (length, arr, visualIndexArr) => {
-    heapify(length, arr, visualIndexArr)
-    for(let top = length; top > 0; top--){
-        arr[top] = deleteMax(top, arr, visualIndexArr)
+    // console.log("before heapify: ", arr)
+    //heap
+    for (let i = Math.floor(length / 2) - 1; i >= 0; i--){
+        heapify(length, i, arr, visualIndexArr);
+    }
+    // console.log("after heapify: ", arr)
+    
+    // One by one extract an element from heap
+    for (let i = length - 1; i > 0; i--) {
+        // Move current root to end
+        swap(arr, 0, i)
+        visualIndexArr.push({l: 0, r: i, heightL: arr[0], heightR: arr[i]})
+        visualIndexArr.push({l: 0, r: i, heightL: arr[0], heightR: arr[i], colorBack: true})
+
+        // call max heapify on the reduced heap
+        heapify(i, 0, arr, visualIndexArr);
     }
 }
 
-const heapify = (length, arr, visualIndexArr) => {
-    for (let childIndex = 1; childIndex < length; childIndex++){
-        let currIndex = childIndex;
-        let parentIndex = childIndex / 2;
-        while (parentIndex >= 0 && arr[parentIndex] < arr[currIndex]) {
-            if(parentIndex === 0) break;
-            swap(arr, parentIndex, childIndex)
-            visualIndexArr.push({l: parentIndex, r: currIndex, heightL: arr[parentIndex], heightR: arr[currIndex]})
-            visualIndexArr.push({l: parentIndex, r: currIndex, heightL: arr[parentIndex], heightR: arr[currIndex], colorBack: true})
+const heapify = (length, index, arr, visualIndexArr) => {
+    // Find largest among root, left child and right child
+    let largest = index; // Initialize largest as root
+    let l = 2 * index + 1;
+    let r = 2 * index + 2; 
 
-            //reassign indices
-            currIndex = parentIndex
-            parentIndex = currIndex / 2
-        }
+    // If left child is larger than root
+    if (l < length && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < length && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest !== index) {
+        swap(arr, index, largest)
+        visualIndexArr.push({l: index, r: largest, heightL: arr[index], heightR: arr[largest]})
+        visualIndexArr.push({l: index, r: largest, heightL: arr[index], heightR: arr[largest], colorBack: true})
+
+        // Recursively heapify the affected sub-tree
+        heapify(length, largest, arr, visualIndexArr);
     }
 }
 
-//delete and return the max
-const deleteMax = (index, arr, visualIndexArr) => {
-    //at the end of heapify and each call of deleteMax, the largest element in the heap will be located at index 1
-    const large = arr[1];
-    const element = arr[index] //element to be relocated in arr[0...index-1] 
-    let currentIndex = 0; //element will be relocated by getting assigned to this index
-    let childIndex = 1;
 
-    while (childIndex < index) {
-        //first get the bigger of the 2 children nodes
-        if (arr[childIndex+1] > arr[childIndex]){
-            childIndex++;
-        }
-
-        //push the element down to next level if less than arr[childIndex]... childIndex < index so element will never
-        // be swapped down to level past index ( arr[index...length-1] )
-        if (element  < arr[childIndex]) {
-            arr[currentIndex] = arr[childIndex] //set childIndex as new currentIndex 
-            currentIndex = childIndex
-            childIndex = 2 * currentIndex
-            visualIndexArr.push({l: childIndex, r: currentIndex, heightL: arr[childIndex], heightR: arr[currentIndex]})
-            visualIndexArr.push({l: childIndex, r: currentIndex, heightL: arr[childIndex], heightR: arr[currentIndex], colorBack: true})
-        }
-        else {
-            arr[currentIndex] = element
-            visualIndexArr.push({l: currentIndex, r: index, heightL: arr[currentIndex], heightR: element})
-            visualIndexArr.push({l: currentIndex, r: index, heightL: arr[currentIndex], heightR: element, colorBack: true})
-            return large
-        }
-    } //stop while loop if iterated past outside the index range
-
-    arr[currentIndex] = element
-    visualIndexArr.push({l: currentIndex, r: index, heightL: arr[currentIndex], heightR: element})
-    visualIndexArr.push({l: currentIndex, r: index, heightL: arr[currentIndex], heightR: element, colorBack: true})
-    return large
-
-}
 
 export default hSort
